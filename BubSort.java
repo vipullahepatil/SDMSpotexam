91942647a14c0c58d5f87e22103d161c6d5600e5
vipullahepatil <vipullahepatil@gmail.com>
2023-02-02 15:14:53 +0530

    after Selection code update

--- BubSort.java
+++ BubSort.java
@@ -19,6 +19,30 @@ public class BubSort {
         }
         System.out.println();
     }
+
+    public static void SSort(int arr[]){
+       
+        for(int i=0;i<arr.length-1;i++){
+            int index=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[index]){
+                    index=j;
+                }
+            }
+            int temp=arr[index];
+            arr[index]=arr[i];
+            arr[i]=temp;
+        }
+        System.out.println("after sort");
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        
+
+
+    }
+
     public static void main(String[] args) {
         int arr[]={48,5,4,85,21,45,63,78};
 
@@ -27,6 +51,7 @@ public class BubSort {
             System.out.print(i+" ");
         }
         System.out.println();
-        BSort(arr);
+        //BSort(arr);
+        SSort(arr);
     }
 }
