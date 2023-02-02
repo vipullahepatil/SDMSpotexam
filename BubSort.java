fae5bf6d7f9cc7172fa9767cefa87ec6f8f5b964
vipullahepatil <vipullahepatil@gmail.com>
2023-02-02 15:04:47 +0530

    After insertion sort update

--- BubSort.java
+++ BubSort.java
@@ -19,6 +19,28 @@ public class BubSort {
         }
         System.out.println();
     }
+
+    public static void Ishort(int arr[]){
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            int key=arr[i];
+            int j=i-1;
+
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j=j-1;
+
+            }
+            arr[j+1]=key;
+        }
+
+        System.out.println("after sort");
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int arr[]={48,5,4,85,21,45,63,78};
 
@@ -27,6 +49,8 @@ public class BubSort {
             System.out.print(i+" ");
         }
         System.out.println();
-        BSort(arr);
+
+        //BSort(arr);
+       Ishort(arr);
     }
 }
